7757c0256d17e514bd1846382ec421a62bbcde06
David B Wetzel <david@familycatproductions.com>
2024-01-03 09:58:01 -0600

    upload template files

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Lab 3b - Declare Your Own Variables
+ * Declare and use a variable in a P5 sketch
+ * 
+ */
+
+//declare a global variable and assign it a number value on the next line:
+
+
+function setup() {
+  createCanvas(600, 400); // creates a drawing canvas
+}
+
+function draw() {
+  // background is drawn first
+  background(200); // light gray
+
+  // call a simple P5 drawing function, such as ellipse() using your new variable(s) in place of arguments
+  
+  
+}
